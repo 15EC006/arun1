825ac6ff671c83a30ffe830b08fdce5958550835
Arun prasath K S <arunprasath176@gmail.com>
2018-03-21 21:38:06 +0530

    Create NUMeo.c

---
+++ NUMeo.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("Enter the number :");
+scanf("%d",&num);
+if(num%2==0)
+printf("Entered number is even");
+else
+printf("Entered number is odd");
+return 0;
+}
