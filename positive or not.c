fc84176a69764601e7d06e7b6d2c0742162c6f51
Arun prasath K S <arunprasath176@gmail.com>
2018-03-21 21:10:25 +0530

    Create positive or not.c

---
+++ positive or not.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("Enter the number:");
+scanf("%d",&a);
+if(a<=0)
+printf("The entered number is Negative");
+else if
+printf("The entered number is Positive");
+else
+printf(Please enter the valid number");
+return 0;
+}
