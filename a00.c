4efb8561462f0fe2456976e1c733832f3e474314
Arun prasath K S <arunprasath176@gmail.com>
2018-07-23 12:09:50 +0530

    Create a00.c

---
+++ a00.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+int n,m,n1,temp=0,c,i;
+printf("enter the number");
+scanf("%d%d",&n,&m);
+for(i=n;i<m;i++)
+{
+    c=n;
+while(n>0)
+{
+n1=n%10;
+temp=temp+(n1*n1*n1);
+n=n/10;
+}
+
+
+if(temp==c)
+{
+printf(" armstrong number");
+}
+else
+{
+printf("not an amstrong");
+}}}
