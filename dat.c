0ca5a2a9a2880a1e914418b5332629558aefae59
Arun prasath K S <arunprasath176@gmail.com>
2018-08-30 14:33:22 +0530

    Create dat.c

---
+++ dat.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+    int b, e;
+
+    int a = 1;
+    scanf("%d", &b);
+    scanf("%d", &e);
+    while(e!=0)
+    {
+    a*=b;
+    --e;
+    }
+    printf("%d",a);
+    }
+
+    
